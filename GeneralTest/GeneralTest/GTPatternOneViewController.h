6c16eaac65a794f3ecc2793ad2765b3e008d26fc
Hayden <i_of_me@hotmail.com>
2013-08-15 11:58:24 +1200

    new test

---
+++ GeneralTest/GeneralTest/GTPatternOneViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTPatternOneViewController.h
+//  GeneralTest
+//
+//  Created by Hayden Young on 15/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTPatternOneViewController : UIViewController
+
+@end
---
+++ GeneralTest/GeneralTest/GTPatternOneViewController.m
@@ -0,0 +1,77 @@
+//
+//  GTPatternOneViewController.m
+//  GeneralTest
+//
+//  Created by Hayden Young on 15/08/13.
+//  Copyright (c) 2013 Hayden Young. All rights reserved.
+//
+
+#import "GTPatternOneViewController.h"
+
+@interface GTPatternOneViewController ()
+
+@end
+
+@implementation GTPatternOneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+	
+	NSArray *titles = @[@"banana", @"square", @"volley", @"pot", @"corn"];
+	
+	CGFloat yPos = 100;
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:titles[0] forState:UIControlStateNormal];
+		[self.view addSubview:button];
+		button.center = CGPointMake(100, yPos);
+		yPos += button.frame.size.height * 1.5;
+	}
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:titles[1] forState:UIControlStateNormal];
+		[self.view addSubview:button];
+		button.center = CGPointMake(100, yPos);
+		yPos += button.frame.size.height * 1.5;
+	}
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:titles[2] forState:UIControlStateNormal];
+		[self.view addSubview:button];
+		button.center = CGPointMake(100, yPos);
+		yPos += button.frame.size.height * 1.5;
+	}
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:titles[3] forState:UIControlStateNormal];
+		[self.view addSubview:button];
+		button.center = CGPointMake(100, yPos);
+		yPos += button.frame.size.height * 1.5;
+	}
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+		[button setTitle:titles[4] forState:UIControlStateNormal];
+		[self.view addSubview:button];
+		button.center = CGPointMake(100, yPos);
+		yPos += button.frame.size.height * 1.5;
+	}
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
